4975e567b2cf4e950f27fedcf71fe3c4343e4361
chandrawat <saksham.mist@gmail.com>
2019-06-23 21:54:08 -0400

    Added Selection Sort implementation

---
+++ DSandAlgorithms/src/com/array/SelectionSortImplementation.java
@@ -0,0 +1,36 @@
+package com.array;
+
+public class SelectionSortImplementation {
+
+	public static void main(String[] args) {
+		
+
+		//int[] a= {3,14,23,56,32,21,40,50,7};
+		String[] a= {"a", "v","x","g","b","c"};
+		int minValue;
+		//int temp= 0;
+		String temp= "";
+		
+		for(int i=0;i<a.length;i++) {				// for each round/cycle
+			minValue=i;								// saving the value of i
+				
+			for(int j=i+1;j<a.length;j++) {			// start to compare the element from next till last
+				//if(a[j]<a[minValue]) {				// if any small value found it keep of replacing the value till smallest value in list
+				if(a[j].compareTo(a[minValue]) <0) {
+					minValue=j;
+				}
+			}
+			temp = a[i];							// once least value found it will swap the value of a[i] to minimum value in current
+			a[i] = a[minValue];
+			a[minValue]= temp;
+			
+		}
+		//for(int n:a) {
+		for(String n:a) {
+			
+			System.out.print(n+ "  ");
+		}
+
+	}
+
+}
