665585f6895733eed16af5d15a0047c58fb91315
chandrawat <saksham.mist@gmail.com>
2019-07-30 00:06:15 -0400

    added more methods

--- DSandAlgorithms/src/com/array/ArrayMethods.java
+++ DSandAlgorithms/src/com/array/ArrayMethods.java
@@ -1,6 +1,9 @@
 package com.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+
+
 
 public class ArrayMethods {
 
@@ -17,10 +20,18 @@ public class ArrayMethods {
 		System.out.println(inString);
 
 		//Create an ArrayList from array
-
+		ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(bType));
+		System.out.println(arrayList);
 		
+		//check if array contains certain value
+		boolean find = Arrays.asList(bType).contains("b");
+		System.out.println(find);
 		
-		System.out.println("Hello World");
+		int[] intA1= {1,2,3};
+		int[] intA2= {4,5};
+		//int[] concatenate = ArrayUtils.
+		
+		//System.out.println("Hello World");
 	}
 
 }
