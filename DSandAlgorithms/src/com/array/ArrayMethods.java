05247e81d406d7ebc36543c1f5b761fbff89181d
chandrawat <saksham.mist@gmail.com>
2019-07-29 22:42:29 -0400

    Implementing all method of array

---
+++ DSandAlgorithms/src/com/array/ArrayMethods.java
@@ -0,0 +1,26 @@
+package com.array;
+
+import java.util.Arrays;
+
+public class ArrayMethods {
+
+	public static void main(String[] args) {
+			
+		// Declaration of array
+		String[] aType = new String[5];
+		String[] bType = {"a","b","c"};
+		String[] cType = new String[] {"a","b","c","d"};
+		
+		//print an array
+		int[] aInt= {1,2,3,4,5};
+		String inString = Arrays.toString(aInt);
+		System.out.println(inString);
+
+		//Create an ArrayList from array
+
+		
+		
+		System.out.println("Hello World");
+	}
+
+}
