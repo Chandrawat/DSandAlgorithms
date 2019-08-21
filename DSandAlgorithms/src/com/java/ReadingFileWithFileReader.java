a239b5707ac9b33e0305fa2cf9ba0f187623074d
Saksham Chandrawat <saksham.mist@gmail.com>
2019-08-21 18:21:18 -0400

    Reading data from a file

--- DSandAlgorithms/src/com/java/ReadingFileWithFileReader.java
+++ DSandAlgorithms/src/com/java/ReadingFileWithFileReader.java
@@ -1,9 +1,39 @@
 package com.java;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadingFileWithFileReader {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		File file =new File("resourceFile.txt");
+		
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader(file);
+			BufferedReader br = new BufferedReader(fileReader);
+			
+			String line;
+			
+			while((line = br.readLine())!=null){
+				System.out.println(line);
+			}
+		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		
+		
 
 	}
 
