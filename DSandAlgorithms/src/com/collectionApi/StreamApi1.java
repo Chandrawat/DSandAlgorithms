6ebc8db7b495c8b4854610a241af82f79224500c
Saksham Chandrawat <saksham.mist@gmail.com>
2019-08-22 15:37:40 -0400

    Updated with Lambda expression

--- DSandAlgorithms/src/com/collectionApi/StreamApi1.java
+++ DSandAlgorithms/src/com/collectionApi/StreamApi1.java
@@ -1,8 +1,8 @@
 package com.collectionApi;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class StreamApi1 {
 
@@ -10,22 +10,56 @@ public class StreamApi1 {
 		
 		List<Integer> al =Arrays.asList(1,2,3,4,5,6);
 		
-		for(int i: al) {
-			System.out.println(i);
-		}
+		/*
+		 * for(int i: al) { System.out.println(i); }
+		 *
+		 * Iterator its= al.iterator(); while(its.hasNext()) {
+		 * System.out.println(its.next()); }
+		 * 
+		 * for(int i=0;i<al.size();i++) { System.out.println(al.get(i)); }
+		 */
 		
-		Iterator its= al.iterator();
-		while(its.hasNext()) {
-			System.out.println(its.next());
-		}
+		
+		/*
+		 * Consumer<Integer> c = new Consumer<Integer>() {
+		 * 
+		 * @Override public void accept(Integer t) {
+		 * 
+		 * 
+		 * System.out.println(t);
+		 * 
+		 * 
+		 * }
+		 * 
+		 * };
+		 */
 		
 		
 		
-		for(int i=0;i<al.size();i++) {
-			System.out.println(al.get(i));
-		}
-
+		//Consumer<Integer> c = (Integer t)-> System.out.println(t);
 		
+		//Consumer<Integer> c = t-> System.out.println(t);
+	
+		//functional interface
+		//internal iterations
+		
+		//al.forEach(i -> System.out.println(i));
+		
+		
+		//al.forEach(c);
+		
+		/*
+		 * al.forEach(new Consumer<Integer>() {
+		 * 
+		 * @Override public void accept(Integer t) {
+		 * 
+		 * 
+		 * System.out.println(t);
+		 * 
+		 * } });
+		 */
+		
+		//al.forEach(t-> System.out.println(t));
+	
 	}
-
 }
